14367c2f32191d5a58dcb8df10eb020ebca003a8
jhaiyno <113760300+jhaiyno@users.noreply.github.com>
2022-10-04 14:29:11 +0700

    pro.sh

---
+++ pro.sh
@@ -0,0 +1,261 @@
+#mau ngapain??
+#Recode yah??
+#Mikir Goblok Pake Otak -_-
+#By TU4N G4L4U
+pkg install figlet -y
+clear
+
+
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+
+figlet -f slant Multi Mining
+
+sleep 1
+echo $red "Downloading Tools"
+sleep 2
+echo $red "Prosesing"
+sleep 1
+echo $red "///////////////"
+sleep 1
+echo $red "/////////////////"
+sleep 1
+echo $red "////////////////////"
+sleep 1
+echo $red "////////////////////////"
+sleep 1
+echo $red "////////////////////////////"
+sleep 1
+echo $red "///////////////////////////////"
+sleep 1
+echo $red "///////////////////////////////////"
+sleep 1
+echo "\033[33;1m<========================================================>"
+sleep 3
+figlet -f slant √Done✓
+sleep 1
+echo "\033[33;1m<========================================================>"
+sleep 2
+echo
+echo
+sleep 1
+echo $purple "_____________________________" 
+echo $red ">>"   $cyan "1.Mining M7M"       
+echo $red ">>"   $cyan "2.Mining RandomX"    
+echo $red ">>"   $cyan "3.Hack FB Masal"    
+echo $red ">>"   $cyan "4.Osif"              
+echo $purple "____________________________" 
+echo                                        
+echo
+sleep 1     
+echo $purple "____________________________"  
+echo $red ">>"   $blue "5.Tools TU4N G4L4U"   
+echo $red ">>"   $blue "6.Toolss B4J1N64N"
+echo $red ">>"   $blue "7.Tools TUAN B4DUT"  
+echo $red ">>"   $blue "8.Tools Sk4ters"     
+echo $purple "____________________________"  
+echo					    
+echo	
+sleep 1     
+echo $purple "____________________________" 
+echo $red ">>"   $white "9.Scan Website"     
+echo $red ">>"   $white "10.WebDav"           
+echo $red ">>"   $white "11.Hack Wifi(Root)"   
+echo $red ">>"   $white "12.Creator Deface"
+echo $purple "____________________________"    
+echo					    
+echo
+sleep 1       
+echo $purple "____________________________"   
+echo $red ">>"   $green "13.DDOS Hammer"      
+echo $red ">>"   $green "14.LITEDDOS"       
+echo $purple "____________________________"  
+echo					     
+echo
+read -p "r00t@Premay " pilih
+if [ $pilih = 1 ]
+then
+clear
+sleep 1
+pkg install python2
+pip install requests
+pip install mechanize
+pkg install git
+git clone https://github.com/wahyuandhika/YahooCloning
+cd YahooCloning
+python2 cloning.py
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+figlet Gtuyy | lolcat
+echo
+echo "Loading...!!"
+echo ""
+sleep 1
+apt install git
+pkg install python2
+pip2 install mechanize
+git clone https://github.com/rickyricko302/fb-crackv1
+cd fb-crackv1
+python2 fb-crack.py
+fi
+
+if [ $pilih = 3 ]
+then
+clear
+figlet -f slant W A I T | lolcay
+echo
+sleep 1
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pilih = 4 ]
+then
+clear
+echo
+echo
+sleep 1
+apt update && apt upgrade
+apt install python2
+pip2 install requests mechanize
+apt install git
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pilih = 5 ]
+then
+clear
+figlet -f slant r00t@TU4MG4L4U | lolcat
+echo
+echo "Sabar............!!!!"
+sleep 1
+pkg install git
+pkg install ruby
+gem install lolcat
+git clone https://github.com/TU4NG4L4U/InstallerV6
+cd InstallerV6
+sh InstallerV6.sh
+fi
+
+if [ $pilih = 6 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo "\033[34;1mLoading......!!!"
+echo ""
+sleep 1
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
+
+if [ $pilih = 7 ]
+then
+clear
+sleep 1
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+cd TOOLSINSTALLERv1
+chmod +x Tuanb4dut.sh
+sh Tuanb4dut.sh
+fi
+
+if [ $pilih = 8 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo "Loading....!!!"
+sleep 1
+pkg install git
+git clone https://github.com/Sk4ters38/SK4T3R5v1
+cd SK4T3R5v1
+sh SK4T3RS.sh
+fi
+
+if [ $pilih = 9 ]
+then
+clear
+sleep 1
+pkg install git
+git clone https://github.com/PhobiaXploit/Zanscan
+chmod +x zanscan.sh
+./zanscan.sh
+fi
+
+if [ $pilih = 10 ]
+then
+clear
+sleep 1
+pkg install git
+git clone https://github.com/TUANB4DUT/WEBDAV
+cd WEBDAV
+sh webdav.sh
+fi
+
+if [ $pilih = 11 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo "Loading....!!!"
+echo ""
+sleep 1
+pkg install git
+git clone https://github.com/derv82/wifite
+cd wifite
+chmod 777 wifite.py
+python2 wifite.py
+./wifite.py
+fi
+
+if [ $pilih = 12 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loding....!!"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if [ $pilih = 13 ]
+then
+clear
+sleep 1
+pkg install python
+pkg install git
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi
+
+if [ $pilih = 14 ]
+then
+clear
+sleep 1
+pkg install python2
+pkg install git
+git clone https://github.com/4L13199/LITEDDOS
+echo $red "Cara Jalanin Tools Nya Ketik : "
+echo $green "python2 liteDDOS.py [ Ip Target ] [ Port ] [ jumlah ping ]"
+echo $green "contoh python2 LITEDDOS.py 148.118.86.37 80 100 "
+echo
+fi
